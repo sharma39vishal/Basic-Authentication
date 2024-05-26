928fe9512caa84d27846100edd84bb9a7c6d764c
Vishal Sharma <85349991+sharma39vishal@users.noreply.github.com>
2024-05-26 22:38:16 +0530

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,11 +3,11 @@ const jwt = require('jsonwebtoken');
 const authMiddleware = (req, res, next) => {
   const token = req.cookies.token;
   if (!token) {
-    return res.status(401).json({ message: 'Authentication failed' });
+    return res.status(401).json({ message: 'Token Missing' });
   }
 
   try {
-    const decoded = jwt.verify(token, 'secret');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded.userId;
     next();
   } catch (error) {
